01dcbb43cfcbe45912c80a31104e953e837e6953
Ajay703 <114392452+Ajay703@users.noreply.github.com>
2023-02-20 23:11:02 +0530

    Add files via upload

---
+++ src/StudentExaminationProject1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExaminationProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentExaminationProject1Application.class, args);
+	}
+
+}
